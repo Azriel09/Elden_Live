6fb2de9614a3c62b388866813371667c9b66211b
Azuriyale <Azuriyale@users.noreply.github.com>
2023-12-03 21:16:22 +0800

    player container and minor fixes

--- src/components/home/home-container.jsx
+++ src/components/home/home-container.jsx
@@ -1,7 +1,14 @@
 import "./home-container-styles.scss";
 import EldenAnimatedBG from "../../assets/elden-ring-header-animated.webm";
 import HoloElden from "../../assets/holo-elden2.png";
+import { useTalentState } from "../../context/talent-context";
+import { useEffect } from "react";
 export default function HomeContainer() {
+  const { setSelectedTalent } = useTalentState();
+  useEffect(() => {
+    setSelectedTalent("");
+  }, []);
+
   return (
     <div className="home-container">
       <div className="header-video-container">
