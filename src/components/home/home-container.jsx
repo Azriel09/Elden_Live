75dd8b8841919412542af76efc6e5fd8d27011ee
Jhan Marmel <jhanmarmel@gmail.com>
2023-12-09 21:12:16 +0800

    added stream selection and fixed errors

--- src/components/home/home-container.jsx
+++ src/components/home/home-container.jsx
@@ -3,15 +3,22 @@ import EldenAnimatedBG from "../../assets/elden-ring-header-animated.webm";
 import HoloElden from "../../assets/holo-elden2.png";
 import { useTalentState } from "../../context/talent-context";
 import { useEffect } from "react";
+import Loading from "../loading/loading";
+import GetLinks from "../../query/fetch_links";
 export default function HomeContainer() {
   const { setSelectedTalent } = useTalentState();
   useEffect(() => {
     setSelectedTalent("");
   }, []);
+  const { status, data, error, isFetching } = GetLinks();
+  if (status === "loading") {
+    return <Loading />;
+  }
 
   return (
     <div className="home-container">
       <div className="header-video-container">
+   
         <div className="header">
           <img src={HoloElden} className="holo-elden" />
         </div>
