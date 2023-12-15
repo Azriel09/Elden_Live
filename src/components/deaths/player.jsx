9222530b7ed8b0ccd22471ace44051f7ed52da08
Jhan Marmel <jhanmarmel@gmail.com>
2023-12-15 20:12:13 +0800

    player now working

--- src/components/deaths/player.jsx
+++ src/components/deaths/player.jsx
@@ -1,9 +1,21 @@
-import { useEffect } from "react"
-
-const sheetID = import.meta.env.VITE_SHEET_ID
-export default function Player() {
- 
-
-
-  return <button style={{width: "100px", height: "40px"}}>GG</button>
+import React from "react";
+import ReactPlayer from "react-player";
+import "./player_styles.scss";
+const sheetID = import.meta.env.VITE_SHEET_ID;
+export default function Player({ selectedStreamLink }) {
+  const ref = React.createRef();
+  const stream_link = selectedStreamLink.replace("watch?v=", "embed/") + "?rel=0";
+  return (<div className="player-container">
+    <div className="react-player-wrapper">
+      <ReactPlayer
+        className="player"
+        ref={ref}
+        url={stream_link}
+        width="100%"
+        height="100%"
+        controls
+      />
+    </div>
+    </div>
+  );
 }
