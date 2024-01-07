5309e8a9ee8dc626c1998db8dbb1a3c4edb3f1c3
Jhan Marmel <jhanmarmel@gmail.com>
2024-01-07 23:10:10 +0800

    minor fixes

--- src/components/boss/boss_bar_graph.jsx
+++ src/components/boss/boss_bar_graph.jsx
@@ -1,28 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
 import GetData from "../../query/fetch_links";
 import Loading from "../loading/loading";
-export default function BossBarChart({ selectedBoss }) {
+export default function BossBarChart({ selectedBoss, data }) {
   const [getCategories, setGenCategories] = useState([]);
-  const data = GetData();
-  if (data === "loading") {
-    return <Loading />;
-  }
-
+  const [allData, setAllData] = useState({});
   //   Formats the data into {Talent: [all cause of deaths]}
-  const allDeaths = Object.entries(data.deaths).map((deaths) => {
-    const tempoObjects = {};
-    // tempoObjects[deaths[0]] = deaths[1].map((death) => {
-    //   return Object.values(death.timestamps_killers);
-    // });
-    const deathsArr = [];
-    deaths[1].map((death) => {
-      deathsArr.push(Object.values(death.timestamps_killers));
+  useEffect(() => {
+    const allDeaths = Object.entries(data).map((deaths) => {
+      const tempoObjects = {};
+      // tempoObjects[deaths[0]] = deaths[1].map((death) => {
+      //   return Object.values(death.timestamps_killers);
+      // });
+      const deathsArr = [];
+      deaths[1].map((death) => {
+        deathsArr.push(Object.values(death.timestamps_killers));
+      });
+      const flattenedArr = deathsArr.flat();
+      tempoObjects[deaths[0]] = flattenedArr;
+      return tempoObjects;
     });
-    const flattenedArr = deathsArr.flat();
-    tempoObjects[deaths[0]] = flattenedArr;
-    return tempoObjects;
-  });
-
-  
+    setAllData(allDeaths);
+    const filteredDeaths = {};
+    // allDeaths.map((data) => {
+    //   Object.entries(data).map((key) => {
+    //     filteredDeaths[key] = key[1].filter(name => name.indexOf(selectedBoss) !== -1)
+    //   });
+    // });
+    console.log(filteredDeaths);
+  }, []);
 }
