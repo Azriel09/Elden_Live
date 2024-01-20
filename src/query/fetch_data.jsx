b2a0cc11695e520a7612bd1f0de49da14dc2a72c
Jhan Marmel <jhanmarmel@gmail.com>
2024-01-20 22:22:08 +0800

    added a new query for talents stats for each boss

--- src/query/fetch_data.jsx
+++ src/query/fetch_data.jsx
@@ -44,15 +44,36 @@ const GetData = () => {
         },
         staleTime: 120000,
       },
+      {
+        queryKey: ["boss_talents_stats"],
+        queryFn: () => {
+          const data = fetch("http://localhost:8000/get_boss_stats", {
+            headers: {
+              apiKey: apiKeyData,
+            },
+          }).then((res) =>
+            res.json(res).then((resdata) => {
+              return resdata;
+            })
+          );
+
+          return data;
+        },
+        initialData: () => {
+          return queryClient.getQueryData(["boss_talents_stats"]);
+        },
+        staleTime: 120000,
+      },
     ],
   });
-  const { isLoading: isLoadingStuff, data: links } = results[0];
-  const { isLoading: isLoadingThings, data: deaths } = results[1];
+  const { isLoading: isLoadingLinks, data: links } = results[0];
+  const { isLoading: isLoadingDeaths, data: deaths } = results[1];
+  const { isLoading: isLoadingBossStats, data: bossStats } = results[2];
   const isLoading = results.some((result) => result.isLoading);
   if (isLoading) {
     return "loading";
   }
-  return { links, deaths };
+  return { links, deaths, bossStats };
 };
 
 export default GetData;
