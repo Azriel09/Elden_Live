646e1f198531b56700dc0cdb3853ff5a044b24b6
Jhan Marmel <jhanmarmel@gmail.com>
2024-01-21 20:46:04 +0800

    talent stats

--- src/components/boss/boss_container.jsx
+++ src/components/boss/boss_container.jsx
@@ -4,16 +4,22 @@ import BossBarChart from "./boss_bar_graph";
 import BossSelection from "./boss_selection";
 import "./boss_styles.scss";
 import { useEffect, useState } from "react";
+
 export default function BossContainer() {
   const [selectedBoss, setSelectedBoss] = useState("Tree Sentinel");
   const data = GetData();
   if (data === "loading") {
     return <Loading />;
   }
+
   return (
     <div className="boss-container">
       <BossSelection setSelectedBoss={setSelectedBoss} />
-      <BossBarChart selectedBoss={selectedBoss} data={data.deaths} />
+      <BossBarChart
+        selectedBoss={selectedBoss}
+        data={data.deaths}
+        stats={data.bossStats}
+      />
     </div>
   );
 }
