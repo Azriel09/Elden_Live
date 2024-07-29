b876d612c454d4b6a3a2a333c8867934958632a4
Jhan Marmel <jhanmarmel@gmail.com>
2024-07-29 16:12:27 +0800

    tab icon

--- src/components/talent/talent_gen.jsx
+++ src/components/talent/talent_gen.jsx
@@ -1,16 +1,31 @@
 import TalentMainContent from "./main_talent_content";
 import "./talent.css";
+import MythPNG from "../../assets/gen_icons/myth_logo.png";
+import promisePNG from "../../assets/gen_icons/promise.png";
+import adventPNG from "../../assets/gen_icons/advent.png";
 export default function TalentTab({ data, selectedTab, setSelectedTab }) {
-  const tabList = ["All", "holoMyth", "holoPromise", "holoAdvent"];
+  const tabList = ["* All", "holoMyth", "holoPromise", "holoAdvent"];
+  const iconList = ["", MythPNG, promisePNG, adventPNG];
   return (
     <div className="talent-tab-container">
       <ul>
         {tabList.map((tab, i) => {
-          return (
-            <li className="talent-tab" key={i}>
-              {tab}
-            </li>
-          );
+          if (i === 0) {
+            return (
+              <li className="talent-tab" key={i}>
+                {tab}
+              </li>
+            );
+          } else {
+            return (
+              <li className="talent-tab" key={i}>
+                <img src={iconList[i]} className="tab-icon"/>{" "}
+                <li className="talent-tab" key={i}>
+                  {tab}
+                </li>
+              </li>
+            );
+          }
         })}
       </ul>
     </div>
