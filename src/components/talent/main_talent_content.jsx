d18bbd97695578460e1b1641fd505153426d0183
Jhan Marmel <jhanmarmel@gmail.com>
2024-07-31 10:43:08 +0800

    talent card images

--- src/components/talent/main_talent_content.jsx
+++ src/components/talent/main_talent_content.jsx
@@ -1,17 +1,28 @@
 import "./talent.css";
 import { useEffect, useState } from "react";
-import GuraPNG from "../../assets/talent_cards/gura_1.png";
+import GuraPNG from "../../assets/talent_cards/gura.png";
+import CalliPNG from "../../assets/talent_cards/calli.png";
+import AmePNG from "../../assets/talent_cards/ame.png";
+import InaPNG from "../../assets/talent_cards/ina.png";
+import KroniiPNG from "../../assets/talent_cards/kronii.png";
+import BibooPNG from "../../assets/talent_cards/biboo.png";
+import IRySPNG from "../../assets/talent_cards/irys.png";
 export default function TalentMainContent({ data, selectedTab }) {
   const [talentList, setTalentLIst] = useState([]);
+  const [imgList, setImgList] = useState([]);
   const holoGens = {
+    allPNGs: [AmePNG, CalliPNG, GuraPNG, InaPNG, IRySPNG, KroniiPNG, BibooPNG],
     holoMyth: [
       "Amelia Watson",
       "Gawr Gura",
       "Ninomae Ina'nis",
       "Mori Calliope",
     ],
+    mythPNGs: [AmePNG, GuraPNG, InaPNG, CalliPNG],
     holoPromise: ["IRyS", "Ouro Kronii"],
+    promisePNGs: [IRySPNG, KroniiPNG],
     holoAdvent: ["Koseki Biboo"],
+    adventPNGs: [BibooPNG],
   };
   useEffect(() => {
     switch (selectedTab) {
@@ -21,15 +32,19 @@ export default function TalentMainContent({ data, selectedTab }) {
             return talent;
           })
         );
+        setImgList(holoGens["allPNGs"]);
         return;
       case "holoMyth":
         setTalentLIst(holoGens["holoMyth"]);
+        setImgList["mythPNGs"];
         return;
       case "holoPromise":
         setTalentLIst(holoGens["holoPromise"]);
+        setImgList(holoGens["promisePNGs"]);
         return;
       case "holoAdvent":
         setTalentLIst(holoGens["holoAdvent"]);
+        setImgList(holoGens["adventPNGs"]);
         return;
     }
   }, [selectedTab]);
@@ -39,7 +54,7 @@ export default function TalentMainContent({ data, selectedTab }) {
       {talentList.map((talent, i) => {
         return (
           <div className="talent-card" key={i}>
-            <img src={GuraPNG} className="talent-img"></img>
+            <img src={imgList[i]} className="talent-img"></img>
             <div className="talent-name">{talent}</div>
           </div>
         );
