75cbbe78bb725166d725b2bc78ad921179803194
Jhan Marmel <jhanmarmel@gmail.com>
2024-08-01 10:53:04 +0800

    some stats now showing uo

--- src/components/talent/talent_container.jsx
+++ src/components/talent/talent_container.jsx
@@ -3,27 +3,38 @@ import GetData from "../../query/fetch_data";
 import Loading from "../loading/loading";
 import TalentTab from "./talent_tab";
 import TalentMainContent from "./main_talent_content";
-import './talent.css'
+import "./talent.css";
 export default function TalentContainer() {
   const [selectedTab, setSelectedTab] = useState("All");
-
+  const [selectedTalent, setSelectedTalent] = useState("");
+  const [selectedData, setSelectedData] = useState();
   const data = GetData();
   if (data === "loading") {
     return <Loading />;
   }
 
   const handleTabChange = (tab) => {
-    console.log(tab);
     setSelectedTab(tab);
+    setSelectedTalent("");
+  };
+  const handleSelectingTalent = (talent) => {
+    setSelectedTalent(talent);
+    setSelectedData(data.deaths[talent]);
   };
   return (
     <div className="talent-container">
-    <TalentTab
-      data={data}
-      handleTabChange={handleTabChange}
-      selectedTab={selectedTab}
-    />
-    <TalentMainContent selectedTab={selectedTab} data={data}/>
+      <TalentTab
+        data={data}
+        handleTabChange={handleTabChange}
+        selectedTab={selectedTab}
+      />
+      <TalentMainContent
+        selectedData={selectedData}
+        selectedTab={selectedTab}
+        data={data}
+        selectedTalent={selectedTalent}
+        handleSelectingTalent={handleSelectingTalent}
+      />
     </div>
   );
 }
--- src/components/talent/talent_stats.jsx
+++ src/components/talent/talent_stats.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
+
 export default function TalentStats({
-  data,
+  selectedData,
   selectedTalent,
-  setSelectedTalent,
+  handleSelectingTalent,
 }) {
-    
+  const [totalStreams, setTotalStreams] = useState();
+  const [totalDeaths, setTotalDeaths] = useState();
+  const [avgDeaths, setAvgDeaths] = useState();
+  useEffect(() => {
+    console.log(selectedData);
+    setTotalStreams(selectedData.length);
+    let totalD = 0;
+    selectedData.map((stream) => {
+      const deaths = Object.values(stream["timestamps_killers"]).length;
+      totalD = totalD + deaths;
+    });
+    setTotalDeaths(totalD);
+    setAvgDeaths(Math.round(totalD / selectedData.length));
+  }, []);
   return (
     <>
-      <button onClick={() => setSelectedTalent("")}>Reset</button>
+      <button onClick={() => handleSelectingTalent("")}>Reset</button>
       <h1>{selectedTalent}</h1>
+      <div>
+        <h4>Total Streams: {selectedData.length}</h4>
+        <h4>Total Deaths: {totalDeaths}</h4>
+        <h4>Average Deaths/Stream: {avgDeaths}</h4>
+      </div>
     </>
   );
 }
